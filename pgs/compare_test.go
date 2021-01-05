eec0e53f855597770520afc59f2a553bc7a41187
Andrew Archibald <andrew@aarchibald.com>
2021-01-04 17:58:38 -0800

    Tee Read into buffer instead of using ReadAt and SectionReader

--- pgs/compare_test.go
+++ pgs/compare_test.go
@@ -37,7 +37,6 @@ func testFileTwoWay(filename string, log io.Writer) (bool, error) {
 	w := NewWriter(c)
 
 	for i := 0; ; i++ {
-		c.Next()
 		p, err := r.Read()
 		if err == io.EOF {
 			break
@@ -48,74 +47,56 @@ func testFileTwoWay(filename string, log io.Writer) (bool, error) {
 		if err := w.Write(p); err != nil {
 			return false, err
 		}
-		eq, err := c.Equal()
-		if err != nil {
-			return false, err
-		}
-		if !eq {
+		if !c.Equal() {
 			if log != nil {
-				src, dst := c.Sections()
+				rbuf, wbuf := c.Buffers()
 				fmt.Fprintf(log, "%s section %d not equal\n", filename, i)
+				fmt.Fprintln(log, "raw:")
 				d := hex.Dumper(log)
-				io.Copy(d, src)
+				io.Copy(d, rbuf)
 				d.Close()
+				fmt.Fprintln(log, "serialized:")
 				d = hex.Dumper(log)
-				io.Copy(d, dst)
+				io.Copy(d, wbuf)
 				d.Close()
 			}
 			return false, nil
 		}
+		c.Reset()
 	}
 	return true, nil
 }
 
 type Comparer struct {
-	r      io.ReaderAt
-	buf    bytes.Buffer
-	off, n int64 // offset of current section in r
+	r          io.Reader
+	rbuf, wbuf bytes.Buffer
 }
 
 var _ io.ReadWriter = (*Comparer)(nil)
 
-func NewComparer(r io.ReaderAt) *Comparer {
-	return &Comparer{r: r}
+func NewComparer(r io.Reader) *Comparer {
+	var c Comparer
+	c.r = io.TeeReader(r, &c.rbuf)
+	return &c
 }
 
 func (c *Comparer) Read(b []byte) (n int, err error) {
-	n, err = c.r.ReadAt(b, c.off+c.n)
-	c.n += int64(n)
-	return
+	return c.r.Read(b)
 }
 
 func (c *Comparer) Write(b []byte) (n int, err error) {
-	return c.buf.Write(b)
+	return c.wbuf.Write(b)
 }
 
-func (c *Comparer) Equal() (bool, error) {
-	r := io.NewSectionReader(c.r, c.off, c.n)
-	var rb [128]byte
-	wb := c.buf.Bytes()
-	for {
-		n, err := r.Read(rb[:])
-		if err == io.EOF {
-			return len(wb) == 0, nil
-		}
-		if err != nil {
-			return false, err
-		}
-		if len(wb) < n || !bytes.Equal(rb[:n], wb[:n]) {
-			return false, nil
-		}
-		wb = wb[n:]
-	}
+func (c *Comparer) Equal() bool {
+	return bytes.Equal(c.rbuf.Bytes(), c.wbuf.Bytes())
 }
 
-func (c *Comparer) Sections() (src, dst io.Reader) {
-	return io.NewSectionReader(c.r, c.off, c.n), &c.buf
+func (c *Comparer) Buffers() (rbuf, wbuf io.Reader) {
+	return &c.rbuf, &c.wbuf
 }
 
-func (c *Comparer) Next() {
-	c.buf.Reset()
-	c.off += c.n
-	c.n = 0
+func (c *Comparer) Reset() {
+	c.rbuf.Reset()
+	c.wbuf.Reset()
 }
